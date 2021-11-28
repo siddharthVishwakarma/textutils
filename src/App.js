19b66030b667246f411b24bd3072033dd4bd32eb
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-11-29 00:42:13 +0530

    update 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light"); //For dark mode
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
         <Textform heading="Enter the text to analyze below" />
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import React from "react";
+// import PropTypes from "prop-types";
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           Textutils
@@ -31,17 +34,26 @@ export default function Navbar() {
               </a>
             </li>
           </ul>
-          <form className="d-flex">
+
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
             <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
+              className="form-check-input"
+              onClick={props.toggleMode}
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
             />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable dark mode
+            </label>
+          </div>
         </div>
       </div>
     </nav>
