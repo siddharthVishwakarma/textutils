8d3edf8afd8c1abcdc7dfcf2d0760a4a1b0c4bb6
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-12-09 20:02:56 +0530

    Alert cls bug solve

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,13 +6,15 @@ export default function Alert(props) {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   };
   return (
-    props.alert && (
-      <div
-        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
-        role="alert"
-      >
-        <strong>{capatilize(props.alert.type)}</strong>:{props.alert.message}
-      </div>
-    )
+    <div style={{ height: "40px" }}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          <strong>{capatilize(props.alert.type)}</strong>:{props.alert.message}
+        </div>
+      )}
+    </div>
   );
 }
